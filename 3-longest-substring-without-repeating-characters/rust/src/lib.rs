1efcf3fa5e64af78d3edc64829b41409ce7803b9
Louis-Quentin NOÉ <louis-quentin.noe@pulsar-project.fr>
2026-03-28 15:05:10 +0100

    ci(leetcode): solution for exercise 3

--- 3-longest-substring-without-repeating-characters/rust/src/lib.rs
+++ 3-longest-substring-without-repeating-characters/rust/src/lib.rs
@@ -1,16 +1,45 @@
-#![allow(dead_code)]
+use std::collections::HashMap;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut occurences: HashMap<char, usize> = HashMap::new();
+    let mut slide_pos: usize = 0;
+    let mut max_len: usize = 0;
+
+    for (i, c) in s.chars().enumerate() {
+        if let Some(&char_index) = occurences.get(&c)
+            && char_index >= slide_pos
+        {
+            slide_pos = char_index + 1;
+        }
+
+        occurences.insert(c, i);
+        max_len = max_len.max(i - slide_pos + 1);
+    }
+
+    max_len as i32
 }
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn unit1() {
+        let str = String::from_str("abcabcbb");
+        assert_eq!(length_of_longest_substring(str.unwrap()), 3);
+    }
+
+    #[test]
+    fn unit2() {
+        let str = String::from_str("bbbbb");
+        assert_eq!(length_of_longest_substring(str.unwrap()), 1);
+    }
+
+    #[test]
+    fn unit3() {
+        let str = String::from_str("pwwkew");
+        assert_eq!(length_of_longest_substring(str.unwrap()), 3);
     }
 }
